9569c2746a289e174ba637b01ec78cd8a974f37c
EGOIST <hi@egoist.dev>
2025-02-05 20:53:52 +0800

    update ignore list

--- utils.ts
+++ utils.ts
@@ -3,8 +3,10 @@ export const shouldSkipDomain = (url: string) => {
 
   return [
     "reddit.com",
+    "www.reddit.com",
     "x.com",
     "twitter.com",
+    "www.twitter.com",
     // TODO: maybe fetch transcript for youtube videos
     "youtube.com",
     "www.youtube.com",
